a41472cf94e1212a1f50554721d59a193b8ebc3c
gitnotv <97412340+gitnotv@users.noreply.github.com>
2022-05-21 11:23:23 +1000

    No unit given int return
    Returns the exact input the user had written if no time unit had been specified.

--- time_package/time_converter.py
+++ time_package/time_converter.py
@@ -1,22 +1,22 @@
-
-
-
 class timify():
     """[_convert : converts your time in s|m|h|d|w|m|y to seconds then returns the result]"""
 
     def _convert(self, query : str):
-        without_unit = query[:-1]
-        raw_unit = query[-1:]
-        if without_unit.isnumeric():
-            unit_dict = {"s" : 1, "h" : 3600, "m" : 60, "d" : 3600 * 24, "w" : 3600 * 24 * 7, "m" : 3600 * 24 * 7 * 4, "y" : 3600 * 24 * 7 * 4 * 12}
-            try:
-                converter = unit_dict[raw_unit]
-            except KeyError:
-                raise KeyError("Recieved invalid time format.")
-            else:
-                result = int(converter) * int(without_unit)
-                return result
-        raise ValueError("Recieved characters instead of an integer.")
+        if query.isnumeric():
+            return query
+        else:
+            without_unit = query[:-1]
+            raw_unit = query[-1:]
+            if without_unit.isnumeric():
+                unit_dict = {"s" : 1, "h" : 3600, "m" : 60, "d" : 3600 * 24, "w" : 3600 * 24 * 7, "m" : 3600 * 24 * 7 * 4, "y" : 3600 * 24 * 7 * 4 * 12}
+                try:
+                    converter = unit_dict[raw_unit]
+                except KeyError:
+                    raise KeyError("Recieved invalid time format.")
+                else:
+                    result = int(converter) * int(without_unit)
+                    return result
+            raise ValueError("Recieved characters instead of an integer.")
 
 
 
