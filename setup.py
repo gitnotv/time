0a0b13daf37209bef145f2ee0fab5fee3139c0a5
gitnotv <97412340+gitnotv@users.noreply.github.com>
2022-05-21 11:04:03 +1000

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+
+import setuptools
+setuptools.setup(name='time_converter',
+version='0.1',
+description='Converts basic time units',
+url='#',
+author='max',
+install_requires=['opencv-python'],
+author_email='',
+packages=setuptools.find_packages(),
+zip_safe=False)
